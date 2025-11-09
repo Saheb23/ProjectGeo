cfcf0ff36b90b2efc097107f15070c7b9c3000bb
Arnab Dutta <116145209+arnabduttamcc@users.noreply.github.com>
2025-11-09 18:14:07 +0530

    code push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { MapComponent } from './map/map';
+import { Home } from './home/home';
 
 export const routes: Routes = [
-    { path: '', component: MapComponent },
+    { path: '', component: Home },
+    { path: 'home', component: Home },
+    { path: 'map', component: MapComponent },
 ];
